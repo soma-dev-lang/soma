fd57deb1dc7162b406beae28932cda5a99fff822
antoine delorme <antoine.delorme@gmail.com>
2026-04-07 21:01:28 +0200

    Untrack .soma_cache and examples/pi_rust.rs (cache + private benchmark file)

--- .soma_cache/native/proj/target/release/deps/soma_native.d
+++
@@ -1,5 +0,0 @@
-/Users/antoine/soma/.soma_cache/native/proj/target/release/deps/soma_native.d: src/lib.rs
-
-/Users/antoine/soma/.soma_cache/native/proj/target/release/deps/libsoma_native.dylib: src/lib.rs
-
-src/lib.rs:
--- .soma_cache/native/proj/target/release/libsoma_native.d
+++
@@ -1,1 +0,0 @@
-/Users/antoine/soma/.soma_cache/native/proj/target/release/libsoma_native.dylib: /Users/antoine/soma/.soma_cache/native/proj/src/lib.rs
